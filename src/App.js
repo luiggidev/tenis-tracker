dc3e3df76306132968fe9f6697007b9bc9f56d3b
Luiggi Annes Bellincanta <65421097+luiggidev@users.noreply.github.com>
2022-02-17 17:03:03 -0300

    refactors tenis tracker

--- src/App.js
+++ src/App.js
@@ -4,47 +4,67 @@ import React, { useState } from "react";
 export default function App() {
   const [countA, setCountA] = useState(0);
   const [countB, setCountB] = useState(0);
+  const [disableButtons, setDisableButtons] = useState(false);
 
-  const outputScore = (count) => {
-    if (count === 0) {
-      return "love";
-    } else if (count === 1) {
-      return 15;
-    } else if (count === 2) {
-      return 30;
-    } else if (count === 3) {
-      return 40;
+  const outputScore = count => {
+    const scoreTypes = {
+      0 : 'love',
+      1: 15,
+      2: 30,
+      3: 40,
+      'default': 'Add point'
     }
-  };
+    return ( scoreTypes[count] ? 'Score: ' + scoreTypes[count] : scoreTypes['default']);
+  }
 
   const resetScore = () => {
+    setDisableButtons(false);
+
     setCountA(0);
     setCountB(0);
   };
 
-  console.log("countA", countA, "countB", countB);
-
-  const compareScore = (countA, countB) => {
-    if ((countA >= 3 || countB >= 3) && countA === countB) {
-      return "Deuce";
-    } else if ((countA > 3 || countB > 3) && countA > countB + 1) {
-      return "Game A";
-    } else if ((countA > 3 || countB > 3) && countB > countA + 1) {
-      return "Game B";
-    } else if ((countA > 3 || countB > 3) && countA > countB) {
-      return "Advantage A";
-    } else if ((countA > 3 || countB > 3) && countB > countA) {
-      return "Advantage B";
+  const maybeDisableButtons = () => {
+    if( disableButtons === false ){
+      setDisableButtons(true);
     }
+  }
+  const compareScore = (countA, countB) => {
+    const largerThenScore = ( countA > 3 || countB > 3 );
+    const largerThenScoreEqual = ( countA >= 3 || countB >= 3 );
+
+    if ( largerThenScoreEqual && countA === countB) {
+      return "Deuce";
+    } else if ( largerThenScore ) {
+        if ( countA > countB + 1) {
+          maybeDisableButtons();
+          return "Game A"; 
+      } else if ( countB > countA + 1) {
+          maybeDisableButtons();
+          return "Game B";
+      } else if ( countA > countB) {
+          return "Advantage A";
+      } else if ( countB > countA) {
+          return "Advantage B";
+      }
+    }
+  
   };
+
+  console.log("countA", countA, "countB", countB);
+  console.log("disableButton: ", disableButtons);
 
   return (
     <div className="App">
-      <button onClick={() => setCountA(countA + 1)}>
-        Score: {outputScore(countA)}
+      <button
+        disabled={disableButtons}
+        onClick={() => setCountA(countA + 1)}>
+        {outputScore(countA)}
       </button>
-      <button onClick={() => setCountB(countB + 1)}>
-        Score: {outputScore(countB)}
+      <button 
+        disabled={disableButtons}
+        onClick={() => setCountB(countB + 1)}>
+        {outputScore(countB)}
       </button>
       <div>{compareScore(countA, countB)}</div>
       <button onClick={() => resetScore()}>Reset</button>
